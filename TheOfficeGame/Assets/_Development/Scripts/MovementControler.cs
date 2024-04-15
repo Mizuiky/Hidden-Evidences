dbb0ef2f07ca7a4964b3dbf88532623198bdce09
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-04-15 17:15:57 -0300

    Set animation when walking

--- TheOfficeGame/Assets/_Development/Scripts/MovementControler.cs
+++ TheOfficeGame/Assets/_Development/Scripts/MovementControler.cs
@@ -7,35 +7,45 @@ public enum speedType
     Slow
 }
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class MovementControler : MonoBehaviour
 {
     [SerializeField]
     private MovementSettings _settings;
+    private Rigidbody2D _rb;
 
+    private AnimationBase _animation;
     private float _speedMultiplier;
     private float _result;
     private float _angle;
 
     private void Start()
     {
-        _speedMultiplier = _settings.normalSpeedMultiplier;    
+        _speedMultiplier = _settings.normalSpeedMultiplier;
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    public void Move(Vector3 movement)
+    public void Init(AnimationBase animationBase)
     {
-        Debug.Log(movement);
+        _animation = animationBase;
+    }
 
-        if(movement != Vector3.zero)
+    public void Move(Vector2 movement)
+    {
+        if (movement != Vector2.zero)
         {
-            transform.position +=  movement * _speedMultiplier * Time.deltaTime;
-            Rotate(movement);
+            _animation.SetWalk(movement.x, movement.y);
+            _rb.MovePosition(_rb.position + movement * _speedMultiplier * Time.deltaTime);       
+            //Rotate(movement);
         }
+
+        _animation.SetLastWalk(movement.x, movement.y);
     }
 
     private void Rotate(Vector3 movement)
     {
-        _angle = Mathf.Atan2(movement.z, movement.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(_angle, 0, 0);
+        _angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, _angle));
     }
 
     public void ChangeSpeed(speedType speedType)
